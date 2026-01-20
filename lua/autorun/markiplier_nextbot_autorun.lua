d53972ad221a0d877c7d385716dc5f758451bb7f
Boomertaters <boomeritaintaters@gmail.com>
2026-01-20 15:15:45 -0500

    Rename and request bot

--- lua/autorun/markiplier_nextbot_autorun.lua
+++ lua/autorun/markiplier_nextbot_autorun.lua
@@ -1,11 +1,13 @@
-local doingContent = CreateConVar( "markiplier_nextbot_content", 0, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "Request markipliers playermodel whenever a bot is spawned?" )
+local doingContent = CreateConVar( "markiplier_nextbot_dynamiccontent", 0, { FCVAR_ARCHIVE, FCVAR_REPLICATED }, "Dynamically request the mark sounds whenever a bot is spawned?" )
 
 if not SERVER then return end
 
 if not doingContent:GetBool() then -- this will only work after a server restart with the new convar!
+    resource.AddWorkshop( "3649571290" ) -- bot ( sounds ) 
     resource.AddWorkshop( "3151172526" ) -- playermodel
 
 end
 
 -- credits to StrawWagen for this code
+
 -- https://steamcommunity.com/id/StrawWagen
